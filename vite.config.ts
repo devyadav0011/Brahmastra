e926fe19dd3278721427cfb299f26d540506a419
Dev Yadav <devy19372@gmail.com>
2026-01-22 19:33:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
+        base: "/Brahmastra/",
+        server: {
+          port: 3000,
+          host: '0.0.0.0',
       },
       plugins: [react()],
       define: {
