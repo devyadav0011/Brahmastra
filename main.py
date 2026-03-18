15e192abf8cff7f787f28539ec60c2dc619ce372
Dev Yadav <devy19372@gmail.com>
2026-03-18 07:30:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -104,7 +104,6 @@ class BRAHMASTRACore:
 
     def trigger_ui_mute(self):
         if self.window:
-            # Injecting JS directly into the Webview context
             js_code = """
             if (window.brahmastra_uplink) {
                 window.brahmastra_uplink.toggleMute();
